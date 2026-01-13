5a4947ca11acaedafc8d29935fe413a9c8b5a8b9
Asim <asim1306iiit@gmail.com>
2026-01-13 19:54:19 +0530

    Reorganize fields and validation annotations..

--- src/main/java/hrms/hrms/dto/request/EmployerRegisterRequest.java
+++ src/main/java/hrms/hrms/dto/request/EmployerRegisterRequest.java
@@ -1,37 +1,36 @@
 package hrms.hrms.dto.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployerRegisterRequest {
-
-	@NotBlank
-	@Size(min = 2, max = 200)
-	private String companyName;
-
-	@NotBlank
-	@Size(max = 255)
-	private String companyWebPage;
-
 	@NotBlank
 	@Email
 	@Size(max = 180)
 	private String email;
 
 	@NotBlank
-	@Size(min = 10, max = 30)
-	private String phoneNumber;
-
-	@NotBlank
 	@Size(min = 6, max = 100)
 	private String password;
+
+	@NotBlank
+	@Size(min = 2, max = 200)
+	private String companyName;
+
+	@NotBlank
+	@Size(min = 10, max = 30)
+	private String phoneNumber;
+	
+	@NotBlank
+	@Size(max = 255)
+	private String companyWebPage;
 
 	@NotBlank
 	@Size(min = 6, max = 100)
