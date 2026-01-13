492ea2667453592aab49b541f62a4a7a1644217a
Asim <asim1306iiit@gmail.com>
2026-01-13 19:51:02 +0530

    update HrmsApplication.java

--- src/main/java/hrms/HrmsApplication.java
+++ src/main/java/hrms/HrmsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HrmsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HrmsApplication.class, args);
 	}
-
 }
